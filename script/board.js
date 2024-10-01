54d42a6c4f783fadee778dab5a855f701798cbe5
Simone Arnold <166645943+ArnoldSimone@users.noreply.github.com>
2024-10-01 10:51:22 +0200

    add template + changes board.js

--- script/board.js
+++ script/board.js
@@ -1,75 +1,32 @@
-
 async function onloadFuncBoard() {
     tasks = Object.values(await loadFromDatabase(`/tasks`));
-    renderTasksBoard(tasks);
+    renderTasksBoard();
 }
 
-function renderTasksBoard(tasks) {
-    renderTasksInProcess();
-    renderTasksTodo();
-    renderTasksAwaitFeedback();
-    renderTasksDone();
-
-
+function renderTasksBoard() {
+    renderTasksByProgress('todo', 'ctn-tasks-todo', 'No tasks To do')
+    renderTasksByProgress('in progress', 'ctn-tasks-in-progress', 'No tasks In Progress')
+    renderTasksByProgress('await feedback', 'ctn-tasks-await-feedback', 'No tasks Awaiting Feedback')
+    renderTasksByProgress('done', 'ctn-tasks-done', 'No tasks Done')
 }
 
-function renderTasksTodo() {
-    let TasksTodoRef = document.getElementById('ctn-tasks-todo');
-    TasksTodoRef.innerHTML = "";
-    let todoTasks = tasks.filter(task => task.progress === "todo");
-    if (todoTasks.length === 0) {
-        TasksTodoRef.innerHTML = getBoardNoTaskTemplate();
+function renderTasksByProgress(progressStatus, containerId, noTaskMessage) {
+    let containerRef = document.getElementById(containerId);
+    containerRef.innerHTML = "";
+    let filteredTasks = tasks.filter(task => task.progress === progressStatus);
+    if (filteredTasks.length === 0) {
+        containerRef.innerHTML = getBoardNoTaskTemplate(noTaskMessage);
     } else {
-        for (let i = 0; i < todoTasks.length; i++) {
-            TasksTodoRef.innerHTML += getBoardTaskTemplate(todoTasks[i]);
+        for (let i = 0; i < filteredTasks.length; i++) {
+            containerRef.innerHTML += getBoardTaskTemplate(filteredTasks[i]);
         }
     }
 }
 
-function renderTasksInProcess() {
-    let taskInProcessRef = document.getElementById('ctn-tasks-in-progress');
-    taskInProcessRef.innerHTML = "";
-    let inProgressTasks = tasks.filter(task => task.progress === "in progress");
-    if (inProgressTasks.length === 0) {
-        taskInProcessRef.innerHTML = getBoardNoTaskTemplate();
-    } else {
-        for (let i = 0; i < inProgressTasks.length; i++) {
-            taskInProcessRef.innerHTML += getBoardTaskTemplate(inProgressTasks[i]);
-        }
-    }
-}
-
-function renderTasksAwaitFeedback() {
-    let taskAwaitFeedbackRef = document.getElementById('ctn-tasks-await-feedback');
-    taskAwaitFeedbackRef.innerHTML = "";
-    let awaitFeedbackTasks = tasks.filter(task => task.progress === "await feedback");
-    if (awaitFeedbackTasks.length === 0) {
-        taskAwaitFeedbackRef.innerHTML = getBoardNoTaskTemplate();
-    } else {
-        for (let i = 0; i < awaitFeedbackTasks.length; i++) {
-            taskAwaitFeedbackRef.innerHTML += getBoardTaskTemplate(awaitFeedbackTasks[i]);
-        }
-    }
-}
-
-function renderTasksDone() {
-    let taskDoneRef = document.getElementById('ctn-tasks-done');
-    taskDoneRef.innerHTML = "";
-    let doneTasks = tasks.filter(task => task.progress === "done");
-    if (doneTasks.length === 0) {
-        taskDoneRef.innerHTML = getBoardNoTaskTemplate();
-    } else {
-        for (let i = 0; i < doneTasks.length; i++) {
-            taskDoneRef.innerHTML += getBoardTaskTemplate(doneTasks[i]);
-        }
-    }
-}
-
-
-function getBoardNoTaskTemplate() {
+function getBoardNoTaskTemplate(message) {
     return `                        
-    <div class="no-tasks-todo d-flex">
-        No tasks To do
+    <div class="ctn-no-tasks d-flex">
+        ${message}
     </div>`;
 }
 
@@ -79,25 +36,48 @@ function getBoardTaskTemplate(task) {
         ${getTaskCategoryTemplate(task.category)}
         <p id="task-title" class="task-title">${task.title}</p>
         <p id="task-description" class="task-description">${task.description}</p>
-        <div class="task-subtasks d-flex-y">
-            <progress max="3" value="2"></progress>
-            <span class="subtasks-count">1/3 Subtasks</span>
-        </div>
+        ${renderTaskProgressBar(task.subtasks)}
         <div class="ctn-task-bottom d-flex-y">
             <div class="ctn-assigned-to mesh d-flex-y">
                 <div class="assigned-to mesh d-flex bg-1">AM</div>
                 <div class="assigned-to mesh d-flex bg-2">BR</div>
                 <div class="assigned-to mesh d-flex bg-3">HI</div>
             </div>
-            <img class="image-prio-board" src="../assets/img/lowsym.png" alt="">
+            ${getImagePrioTemplate(task.priority)}
         </div>
     </div>`;
 }
 
+function getImagePrioTemplate(priority) {
+    const priortyImages = {
+        'low': '../assets/img/lowsym.png',
+        'medium': '../assets/img/mediumsym.png',
+        'urgent': '../assets/img/urgentsym.png'
+    }
+    return `<img class="image-prio-board" src="${priortyImages[priority]}" alt="">`
+}
+
+function renderTaskProgressBar(subtasks) {
+    if (!subtasks || subtasks.length === 0) {
+        return '';
+    } else {
+        let totalSubtasks = subtasks.length;
+        let completedSubtasks = subtasks.filter(subtask => subtask.completed == true).length;
+        let progressBar = getTaskProgressBarTemplate(totalSubtasks, completedSubtasks);
+        return progressBar;
+    }
+}
+
+function getTaskProgressBarTemplate(totalSubtasks, completedSubtasks) {
+    return `        
+            <div class="task-subtasks d-flex-y">
+                <progress max="${totalSubtasks}" value="${completedSubtasks}"></progress>
+                <span class="subtasks-count">${completedSubtasks}/${totalSubtasks} Subtasks</span>
+            </div>`;
+}
 
 function getTaskCategoryTemplate(category) {
     return `<p id="task-category" class="task-category ${category == 'User Story' ? 'bg-user-story' : 'bg-technical-task'}">${category}</p>`
-
 }
 
 function closeDetailTaskOverlay() {
