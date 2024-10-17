b172b30e7fc12e9edf863d485ab21a8cf5be1cbb
Ricardo <ricardo-pinto@web.de>
2024-10-17 17:54:53 +0200

    sidebar-new-code

--- script/sidebar.js
+++ script/sidebar.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // /**
 //  * Initializes the page by loading the HTML content and setting the active menu.
 //  * This function is called when the page is loaded to ensure the user sees the correct view.
@@ -76,3 +77,78 @@
 //   await initPage();
 //   setupMenuClickListeners();
 // }
+=======
+/**
+ * Sets the background color of the menu based on the current URL.
+ * Calls the appropriate function to change the background color 
+ * for the corresponding menu item.
+ */
+function menuColor() {
+  const currentURL = window.location.href;
+  if (currentURL === 'http://127.0.0.1:5502/html/summary.html') {
+    summaryBackgroundColor();
+  } else if (currentURL === 'http://127.0.0.1:5502/html/addTask.html') {
+    addTaskBackgroundColor();
+  } else if (currentURL === 'http://127.0.0.1:5502/html/board.html') {
+    boardBackgroundColor();
+  } else if (currentURL === 'http://127.0.0.1:5502/html/contacts.html') {
+    contactsBackgroundColor();
+  };
+}
+
+
+/**
+ * Toggles the 'active' class on the summary menu item.
+ * Adds the 'active' class if it is not present, and removes it if it is present.
+ */
+function summaryBackgroundColor() {
+  let menuColor = document.getElementById('menu-summary');
+  if (menuColor.classList.contains('active')) {
+    menuColor.classList.remove('active');
+  } else {
+    menuColor.classList.add('active');
+  };
+}
+
+
+/**
+ * Toggles the 'active' class on the addTask menu item.
+ * Adds the 'active' class if it is not present, and removes it if it is present.
+ */
+function addTaskBackgroundColor() {
+  let menuColor = document.getElementById('menu-addTask');
+  if (menuColor.classList.contains('active')) {
+    menuColor.classList.remove('active');
+  } else {
+    menuColor.classList.add('active');
+  };
+}
+
+
+/**
+ * Toggles the 'active' class on the board menu item.
+ * Adds the 'active' class if it is not present, and removes it if it is present.
+ */
+function boardBackgroundColor() {
+  let menuColor = document.getElementById('menu-board');
+  if (menuColor.classList.contains('active')) {
+    menuColor.classList.remove('active');
+  } else {
+    menuColor.classList.add('active');
+  };
+}
+
+
+/**
+ * Toggles the 'active' class on the contacts menu item.
+ * Adds the 'active' class if it is not present, and removes it if it is present.
+ */
+function contactsBackgroundColor() {
+  let menuColor = document.getElementById('menu-contacts');
+  if (menuColor.classList.contains('active')) {
+    menuColor.classList.remove('active');
+  } else {
+    menuColor.classList.add('active');
+  };
+}
+>>>>>>> Stashed changes
